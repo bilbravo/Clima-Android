a23a8e535547f330946a2d92c3dedf473c38548c
bilbravo <bilbravo@gmail.com>
2020-02-08 21:25:08 -0500

    get location changed status, permission request, etc

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -1,24 +1,37 @@
 package com.londonappbrewery.climapm;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.net.Authenticator;
 
 
 public class WeatherController extends AppCompatActivity {
 
     // Constants:
+    final int REQUEST_CODE = 123;
     final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
     // App ID to use OpenWeather data
-    final String APP_ID = "e72____PLEASE_REPLACE_ME_____13";
+    final String APP_ID = "b106c5d4a4af9654c4c342dda9cef34b";
     // Time between location updates (5000 milliseconds or 5 seconds)
     final long MIN_TIME = 5000;
     // Distance between location updates (1000m or 1km)
     final float MIN_DISTANCE = 1000;
 
     // TODO: Set LOCATION_PROVIDER here:
+    String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER; //NETWORK_PROVIDER;
 
 
 
@@ -29,6 +42,8 @@ public class WeatherController extends AppCompatActivity {
     TextView mTemperatureLabel;
 
     // TODO: Declare a LocationManager and a LocationListener here:
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
 
 
     @Override
@@ -50,6 +65,13 @@ public class WeatherController extends AppCompatActivity {
 
 
     // TODO: Add onResume() here:
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Clima", "onResume() called");
+        Log.d("Clima", "Getting weather for current location");
+        getWeatherForCurrentLocation();
+    }
 
 
 
@@ -58,10 +80,61 @@ public class WeatherController extends AppCompatActivity {
 
 
     // TODO: Add getWeatherForCurrentLocation() here:
+    private void getWeatherForCurrentLocation() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Clima", "onLocationChanged() callback received");
+                String longitude = String.valueOf(location.getLongitude());
+                String latitude = String.valueOf(location.getLatitude());
+
+                Log.d("Clima", "longitude is: " + longitude);
+                Log.d("Clima", "latitude is: " + latitude);
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("Clima", "onPRoviderDisabled() callback received");
+            }
+        };
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
 
 
+            return;
+        }
 
-    // TODO: Add letsDoSomeNetworking(RequestParams params) here:
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if( requestCode == REQUEST_CODE) {
+            if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Clima", "onRequestPermissionsResult(): permission granted!");
+                getWeatherForCurrentLocation();
+            } else {
+                Log.d("Clima", "Permission denied!");
+            }
+        }
+    }
+
+// TODO: Add letsDoSomeNetworking(RequestParams params) here:
 
 
 
